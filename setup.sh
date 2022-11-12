044cd4319ff45f56b730f0b242fbdf1742ac1566
ShawonAshraf <shawon13@live.com>
2022-11-12 06:55:31 +0100

    just keep reqs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
-python -m venv venv && \
- source venv/bin/activate && \
- pip install -r requirements.txt -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install -r requirements.txt -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+ 
